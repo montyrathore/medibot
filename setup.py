10091541bf67c776478deb20209adc161ebcbdcc
Jatin Rathour <114429482+montyrathore@users.noreply.github.com>
2025-05-16 13:51:31 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical_Chat_Bot',
+    version= '0.0.0',
+    author= 'Jatin Rathour',
+    author_email= 'rathorejatin2550@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
